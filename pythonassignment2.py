610642023c40b61d82360b3a9fabc494773294de
vishwapriyai <98021289+vishwapriyai@users.noreply.github.com>
2022-01-20 14:19:21 +0530

    assignment 2 is submitted

---
+++ pythonassignment2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+n=int(input("length of the list"))
+list=[]
+for i in range(n):
+    element=input("enter your name")
+    list.append(element)
+print("details:",list)
+for i in range(n):
+    element=int(input("enter your age"))
+    list.append(element)
+print("details:",list)
+for i in range(n):
+    element=input("enter your destigination")
+    list.append(element)
+print("details:",list)
+for i in range(n):
+    element=input("enter your qualification")
+    list.append(element)
+print("details:",list)
+for i in range(n):
+    element=input("enter the job that your are interested in")
+    list.append(element)
+print("details:",list)
+
+
+
+    
+
+
+
+
